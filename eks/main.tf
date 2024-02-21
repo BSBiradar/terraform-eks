9c26f9e1617a90e2665625b452249c0273736872
BSBiradar <biradarb7766@gmail.com>
2024-02-21 19:12:39 +0530

    eks hcl updated

---
+++ eks/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+    region = "var.region"
+  
+}
+
+resource "aws_eks_cluster" "cbz_eks" {
+  name     = "cbz-cluster"
+  role_arn = "var.aws_iam_role_arn"
+  version = "1.29"
+
+
+
+  vpc_config {
+    subnet_ids = "var.subnet_ids"
+  }
+
+}
+
+resource "aws_eks_node_group" "node" {
+  cluster_name    = "cbz-cluster"
+  node_group_name = "cbz-node"
+  node_role_arn   = "var.aws_node_role_arn"
+  subnet_ids      = "var.subnet_id"
+  ami_type = "var.ami"
+  instance_types = "t3.medium"
+  disk_size = "20"
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+
+}
---
+++ eks/var.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "aws_iam_role_arn" {
+  default = "arn:aws:iam::344118038900:role/my-Eksrole"
+}
+
+variable "subnet_ids" {
+  default = ["subnet-00176d90cad22ee1a","subnet-046008e21d6b01a4c","subnet-0e82cc092ba027788"]
+}
+
+variable "aws_node_role_arn" {
+    default = "arn:aws:iam::344118038900:role/eksnodepolicy"
+}
+
+variable "ami" {
+    default = "ami-0e670eb768a5fc3d4"
+  
+}
